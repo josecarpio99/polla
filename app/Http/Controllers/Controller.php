bdb16eed185e90816422432d0b0758a13e1f40d4
jose chirino <josechirinocarpio@gmail.com>
2022-11-07 17:02:54 -0400

    add core to base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use app\Libraries\Core;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    public $core;
+
+    public function __construct()
+    {
+        /** define Core as global Library */
+        $this->core = new Core();
+    }
+
+    public function missingMethod()
+    {
+        return $this->core->setResponse();
+    }
 }
