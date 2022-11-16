c1a92abaaa8721d3fd4e408684d46855ace04450
jose chirino <josechirinocarpio@gmail.com>
2022-11-16 14:03:26 -0400

    fixed route middlewares

--- routes/v1/all.php
+++ routes/v1/all.php
@@ -16,15 +16,13 @@ $router->group(['middleware' => 'auth'], function () use ($router) {
     /* logout user */
     $router->post('/logout', [ 'as' => 'logout', 'uses' => 'AuthController@logout']);
 
-    /* refresh token */
-    // $router->get('/refresh-token', [ 'as' => 'refreshToken', 'uses' => 'AuthController@refresh']);
 
-    $router->group(['middleware' => 'role:superadmin'], function () use ($router) {
+    $router->group(['prefix' => 'users', 'as' => 'users'], function () use ($router) {
+        $router->get('/', ['as' => 'index', 'uses' => 'UserController@index']);
+        $router->get('/pos', ['as' => 'pos', 'uses' => 'UserController@pos']);
+        $router->get('/{id}', ['as' => 'show', 'uses' => 'UserController@show']);
 
-        $router->group(['prefix' => 'users', 'as' => 'users'], function () use ($router) {
-            $router->get('/', ['as' => 'index', 'uses' => 'UserController@index']);
-            $router->get('/pos', ['as' => 'pos', 'uses' => 'UserController@pos']);
-            $router->get('/{id}', ['as' => 'show', 'uses' => 'UserController@show']);
+        $router->group(['middleware' => 'role:superadmin'], function () use ($router) {
             $router->post('/', ['as' => 'store', 'uses' => 'UserController@store']);
             $router->put('/{id}', ['as' => 'update', 'uses' => 'UserController@update']);
             $router->delete('/{id}', ['as' => 'destroy', 'uses' => 'UserController@destroy']);
@@ -32,40 +30,51 @@ $router->group(['middleware' => 'auth'], function () use ($router) {
             $router->post('add-pos', ['uses' => 'AssignPosToUserController']);
         });
 
-        $router->group(['prefix' => 'plays', 'as' => 'plays'], function () use ($router) {
-            $router->get('/', ['as' => 'index', 'uses' => 'PlayController@index']);
-            $router->get('/{id}', ['as' => 'show', 'uses' => 'PlayController@show']);
+    });
+
+    $router->group(['prefix' => 'plays', 'as' => 'plays'], function () use ($router) {
+        $router->get('/', ['as' => 'index', 'uses' => 'PlayController@index']);
+        $router->get('/{id}', ['as' => 'show', 'uses' => 'PlayController@show']);
+
+        $router->group(['middleware' => 'role:superadmin'], function () use ($router) {
             $router->post('/', ['as' => 'store', 'uses' => 'PlayController@store']);
             $router->put('/{id}', ['as' => 'update', 'uses' => 'PlayController@update']);
             $router->delete('/{id}', ['as' => 'destroy', 'uses' => 'PlayController@destroy']);
             $router->post('/{id}/toggle', ['as' => 'store', 'uses' => 'TogglePlayStatusController']);
-
-            $router->group(['prefix' => '{playId}/tickets', 'as' => 'tickets'], function () use ($router) {
-                $router->get('/', ['as' => 'store', 'uses' => 'TicketController@index']);
-                $router->get('/{id}', ['as' => 'store', 'uses' => 'TicketController@show']);
-                $router->post('/', ['as' => 'store', 'uses' => 'TicketController@store']);
-                $router->put('/{id}', ['as' => 'store', 'uses' => 'TicketController@update']);
-                $router->delete('/{id}', ['as' => 'store', 'uses' => 'TicketController@destroy']);
-            });
-
         });
 
-        $router->get('/settings', ['uses' => 'SettingController@index']);
-        $router->put('/settings', ['uses' => 'SettingController@update']);
-
-        $router->get('/racetracks', ['uses' => 'RaceTrackController@index']);
-    });
-
-    // TICKETS
-    $router->group(['prefix' => 'plays', 'as' => 'plays'], function () use ($router) {
         $router->group(['prefix' => '{playId}/tickets', 'as' => 'tickets'], function () use ($router) {
             $router->get('/', ['as' => 'store', 'uses' => 'TicketController@index']);
             $router->get('/{id}', ['as' => 'store', 'uses' => 'TicketController@show']);
             $router->post('/', ['as' => 'store', 'uses' => 'TicketController@store']);
-            $router->put('/{id}', ['as' => 'store', 'uses' => 'TicketController@update']);
-            $router->delete('/{id}', ['as' => 'store', 'uses' => 'TicketController@destroy']);
+
+            $router->group(['middleware' => 'role:superadmin'], function () use ($router) {
+                $router->put('/{id}', ['as' => 'store', 'uses' => 'TicketController@update']);
+                $router->delete('/{id}', ['as' => 'store', 'uses' => 'TicketController@destroy']);
+            });
         });
+
     });
+
+    $router->group(['middleware' => 'role:superadmin'], function () use ($router) {
+        $router->get('/settings', ['uses' => 'SettingController@index']);
+        $router->put('/settings', ['uses' => 'SettingController@update']);
+    });
+
+
+    $router->get('/racetracks', ['uses' => 'RaceTrackController@index']);
+
+
+    // TICKETS
+    // $router->group(['prefix' => 'plays', 'as' => 'plays'], function () use ($router) {
+    //     $router->group(['prefix' => '{playId}/tickets', 'as' => 'tickets'], function () use ($router) {
+    //         $router->get('/', ['as' => 'store', 'uses' => 'TicketController@index']);
+    //         $router->get('/{id}', ['as' => 'store', 'uses' => 'TicketController@show']);
+    //         $router->post('/', ['as' => 'store', 'uses' => 'TicketController@store']);
+    //         $router->put('/{id}', ['as' => 'store', 'uses' => 'TicketController@update']);
+    //         $router->delete('/{id}', ['as' => 'store', 'uses' => 'TicketController@destroy']);
+    //     });
+    // });
 
 
 
